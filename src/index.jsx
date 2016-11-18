d75631d433cf3140c30d047822dff7ac0621a4bc
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-17 23:19:02 -0800

    feat(components): Set up index & router

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+
+import App from './components/App';
+import store from './data/store';
+
+// Route config
+const routes = (
+  <Route path="/" component={App}>
+  </Route>
+);
+
+// Render
+render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.querySelector('.mount')
+);
