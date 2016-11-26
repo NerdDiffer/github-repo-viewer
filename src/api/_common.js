eda391c46e194fa66f17385d10e768a2509a9ea3
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-25 18:50:56 -0800

    refactor(api): Remove debug helper from from preprocessresponse

--- src/api/_common.js
+++ src/api/_common.js
@@ -1,19 +1,5 @@
 export const BASE_URL = 'https://api.github.com';
 
-const logErrToConsole = response => {
-  const keys = Object.keys(response);
-  console.log('keys on response object:\n', keys);
-
-  const { status, statusText, headers, url, body, timeout } = response;
-
-  console.log('url: %s', url);
-  console.log('status: %s', status);
-  console.log('statusText: %s', statusText);
-  console.log('timeout: %s', timeout);
-  console.log('headers:\n', headers);
-  console.log('body:\n', body);
-};
-
 const extractNextPageUrl = response => {
   const link = response.headers.get('link');
   if (!link) { return null; }
@@ -24,11 +10,10 @@ const extractNextPageUrl = response => {
   return nextLink.split(';')[0].slice(1, -1)
 }
 
-export const preProcessResponse = (response, { debug = false } = {}) => (
+export const preProcessResponse = response => (
   response.json()
     .then(json => {
       if (response.status >= 400) {
-        if (debug) { logErrToConsole(response); }
         throw new Error(json);
       } else {
         const nextPageUrl = extractNextPageUrl(response);
