bdc3207914fd0ef15c9f4d5710674e3b294388bd
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-26 10:52:20 -0800

    feat(api): fetch 100 repos by default

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import { BASE_URL, preProcessResponse } from './_common';
 
 // GET /users/:username/repos
 export const fetchUserRepos = username => {
-  const url = `${BASE_URL}/users/${username}/repos`;
+  const url = `${BASE_URL}/users/${username}/repos?per_page=100`;
   const config = {
     header: {
       Accept: 'application/vnd.github.v3+json'
