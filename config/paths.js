f8f9b6f326a05825157e533909f0041b215bd22a
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-27 10:34:52 -0800

    build(styles): Move semantic-ui files out of src/
    This is not application code, doesn't belong in src/ directory. Also,
    code search is easier with less stuff to search through.

--- config/paths.js
+++ config/paths.js
@@ -5,7 +5,7 @@ const PATHS = {
   SRC:         ROOT + '/src',
   PUBLIC:      ROOT + '/public',
   BUILD:       ROOT + '/public/build',
-  STYLES_DIST: ROOT + '/src/styles/dist'
+  STYLES_DIST: ROOT + '/semantic-ui/dist'
 };
 
 module.exports = PATHS;
