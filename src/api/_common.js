df331fddcc58c3fccc716f1de499f0c58ae4e475
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-26 16:30:47 -0800

    feat(api): Add api call for fetching user information
    refactor(api): Move fetch configuration to _common

--- src/api/_common.js
+++ src/api/_common.js
@@ -1,5 +1,11 @@
 export const BASE_URL = 'https://api.github.com';
 
+export const fetchConfig = {
+  header: {
+    Accept: 'application/vnd.github.v3+json'
+  }
+};
+
 const extractNextPageUrl = response => {
   const link = response.headers.get('link');
   if (!link) { return null; }
--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,17 @@
-import { BASE_URL, preProcessResponse } from './_common';
+import { BASE_URL, preProcessResponse, fetchConfig as config } from './_common';
 
 // GET /users/:username/repos
 export const fetchUserRepos = username => {
   const url = `${BASE_URL}/users/${username}/repos?per_page=100`;
-  const config = {
-    header: {
-      Accept: 'application/vnd.github.v3+json'
-    }
-  };
-
   return fetch(url, config)
     .then(response => preProcessResponse(response))
     .then(({ json, nextPageUrl }) => ({ repos: json, nextPageUrl }));
+};
+
+// GET /users/:username
+export const fetchUser = username => {
+  const url = `${BASE_URL}/users/${username}`;
+  return fetch(url, config)
+    .then(response => preProcessResponse(response))
+    .then(({ json }) => ({ user: json }));
 };
