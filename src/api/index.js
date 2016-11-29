4bcdf94b622f6dca05505ba5ed3b525c8ff41496
Rafael Espinoza <nerddiffer@gmail.com>
2016-11-28 19:29:28 -0800

    feat(api): Fetch repos, sorted by updated_at, desc

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,15 @@
+import { stringify } from 'querystring';
 import { BASE_URL, preProcessResponse, fetchConfig as config } from './_common';
 
 // GET /users/:username/repos
 export const fetchUserRepos = username => {
-  const url = `${BASE_URL}/users/${username}/repos?per_page=100`;
+  const params = stringify({
+    per_page: 100,
+    sort: 'updated',
+    direction: 'desc'
+  });
+
+  const url = `${BASE_URL}/users/${username}/repos?${params}`;
   return fetch(url, config)
     .then(response => preProcessResponse(response))
     .then(({ json, nextPageUrl }) => ({ repos: json, nextPageUrl }));
